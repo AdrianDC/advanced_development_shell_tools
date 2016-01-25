f00e42201844551c8f0612a74743d642f84bc06f
AdrianDC <radian.dc@gmail.com>
2016-01-25 20:02:10 +0100

    Scripts update 25/01/2016
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ Server/BuildNightly121.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+source /media/adriandc/AndroidDev/Server/Vars.rc;
+BuildLog="$ScriptsLog.CM121.log";
+
+# Launch Mode
+BuildMode="manual";
+if [ ! -z "$1" ]; then
+  BuildMode="$@";
+fi;
+
+# Compilation Script
+cd $ScriptsDir;
+source ./android_choose_rom.sh 1 y y 2>&1 | tee $BuildLog;
+source ./android_auto_nightlyLP.sh "automatic" "$BuildMode" 2>&1 | tee -a "$BuildLog";
+
+# Update script logs
+source $ServerDir/LogsSync.sh;
+
+# PushBullet Notification
+BuildSuccess=$(grep -a "make completed successfully" $BuildLog | uniq);
+if [ ! -z "$BuildSuccess" ]; then
+  PushBulletComment="CM12.1 ROM ready !";
+else
+  PushBulletComment="CM12.1 ROM failed.";
+fi;
+notify-send "$PushBulletComment";
+curl --header "Access-Token: $PushBulletToken" \
+     --header "Content-Type: application/json" \
+     --data-binary "{\"body\":\"$PushBulletComment\",\"title\":\"\",\"type\":\"note\",\"url\":\"$PushBulletNoteUrl\",\"created\":\"$(date +%s)\", \"active\":\"true\",\"sender_name\":\"$PushBulletUser\"}" \
+     --request POST https://api.pushbullet.com/v2/pushes >/dev/null;
+
+# CronTab End
+if [ -z "$1" ]; then
+  read key;
+fi;
--- android_sign_zip.sh
+++ android_sign_zip.sh
@@ -10,10 +10,14 @@ echo "";
 printf " > ";
 read -e ZipFile;
 ZipFile=${ZipFile//[\' ]/};
-TargetFile="$ZipFile.signed.zip";
+BackupFile="$ZipFile.original.zip";
+NewSigFile="$ZipFile.signed.zip";
 
-if [ -f "$TargetFile" ]; then
-  rm -f $TargetFile;
+if [ -f "$BackupFile" ]; then
+  rm -f "$BackupFile";
+fi;
+if [ -f "$NewSigFile" ]; then
+  rm -f "$NewSigFile";
 fi;
 
 echo "";
@@ -21,13 +25,20 @@ echo " [ Signing the flashable zip ]";
 echo "";
 
 SignApkDir=$ScriptDir/android_signapk;
-java -jar $SignApkDir/signapk-cm121.jar -w $SignApkDir/testkey.x509.pem $SignApkDir/testkey.pk8 $ZipFile $TargetFile;
+java -jar $SignApkDir/signapk-cm121.jar -w $SignApkDir/testkey.x509.pem $SignApkDir/testkey.pk8 $ZipFile $NewSigFile;
 
-echo "";
-echo "  $(basename $TargetFile) created.";
-echo "";
+if [ -f "$NewSigFile" ]; then
+
+  mv "$ZipFile" "$BackupFile";
+  mv "$NewSigFile" "$ZipFile";
+
+  echo "";
+  echo "  $(basename $ZipFile) created.";
+  echo "";
+
+fi;
+
 TimeDiff=$(($(date +%s)-$TimeStart));
-
 echo "";
 echo " [ Done in $TimeDiff secs ]";
 echo "";
