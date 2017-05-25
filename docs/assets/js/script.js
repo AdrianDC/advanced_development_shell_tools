74a2451509df5cf9fb8107e8c7539a7a9e11b410
Adrian DC <radian.dc@gmail.com>
2017-05-25 17:38:53 +0200

    shell_tools: Add Jekyll anchor headers and links
     * Based upon the changes by SimplGy:
        https://gist.github.com/SimplGy/a229d25cdb19d7f21231
    
    Change-Id: I074fb13215f520ed1ad203385d4cd79c4d59a2cc

---
+++ docs/assets/js/script.js
@@ -0,0 +1,32 @@
+// Function: Anchor links creations
+function anchorizejekyll()
+{
+  'use strict';
+
+  var headingNodes = [];
+  var results;
+  var link;
+  var tags = ['h2', 'h3', 'h4', 'h5', 'h6'];
+
+  tags.forEach(function(tag){
+    results = document.getElementsByTagName(tag);
+    Array.prototype.push.apply(headingNodes, results);
+  });
+
+  headingNodes.forEach(function(node){
+    link = document.createElement('a');
+    link.innerHTML = '&nbsp;&infin;';
+    link.href = '#' + node.getAttribute('id');
+    link.style.visibility = "hidden";
+    node.className = 'anchor-item';
+    node.appendChild(link);
+  });
+
+}
+
+// Page loaded event
+window.onload = function()
+{
+  // Start anchor links creations
+  anchorizejekyll();
+}
