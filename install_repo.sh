0d417fbc5bb2ee3ca6d3ae861c5c75ea71e2a85f
Adrian DC <radian.dc@gmail.com>
2017-09-24 23:10:31 +0200

    install_repo: Cleanup unused old packages list
    Change-Id: Id75ecf56899145ca2ff5e727d329eadb7f200162
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- install_repo.sh
+++ install_repo.sh
@@ -43,9 +43,6 @@ sudo apt-get install --yes openjdk-8-jdk;
 java -version;
 
 # Install generic required packages
-#sudo apt-get install --yes git-core gnupg ccache lzop flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5 lib32z1 lib32ncurses5-dev x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python;
-#sudo apt-get install --yes mingw32 lib32bz2-dev lib32bz2-1.0;
-#sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so;
 sudo apt-get install --yes bison g++-multilib git gperf libxml2-utils make zlib1g-dev:i386 zip;
 sudo apt-get -o Dpkg::Options::='--force-overwrite' install --yes zlib1g-dev:i386;
 
